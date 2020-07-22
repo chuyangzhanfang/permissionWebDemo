6852a81ad29492cffe7ec7c934e0090835dd2c80
sunjialin02 <sunjialin02@meituan.com>
2020-07-22 10:01:51 +0800

    add log model

---
+++ src/main/java/com/jiantou/demo/controller/SysLogController.java
@@ -0,0 +1,36 @@
+package com.jiantou.demo.controller;
+
+import com.jiantou.demo.service.SysLogService;
+import com.jiantou.demo.util.PageUtils;
+import com.jiantou.demo.util.R;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * 系统日志
+ */
+@RestController
+@RequestMapping("/sys/user")
+public class SysLogController {
+
+    @Autowired
+    private SysLogService sysLogService;
+
+    /**
+     * 列表
+     */
+    @ResponseBody
+    @RequestMapping("/list")
+    @RequiresPermissions("sys:log:list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = sysLogService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+}
